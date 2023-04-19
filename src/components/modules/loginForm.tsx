7af4ecfaf5b1b20b30d570ccbd8913b9665b565c
surfboy2000 <52144375+surfboy2000@users.noreply.github.com>
2023-04-19 23:55:37 +0800

    Update loginForm.tsx

--- src/components/modules/loginForm.tsx
+++ src/components/modules/loginForm.tsx
@@ -56,13 +56,13 @@ const LoginForm: React.FC = () => {
     <div className="z-10 flex justify-center  self-center">
       <div className="w-100 mx-auto rounded-2xl bg-white p-12 ">
         <div className="mb-4">
-          <h3 className="text-2xl font-semibold text-gray-800">Sign In </h3>
-          <p className="text-gray-500">Please sign in to your account.</p>
+          <h3 className="text-2xl font-semibold text-gray-800">登陆 </h3>
+          <p className="text-gray-500">请登陆你的账号</p>
         </div>
         <form className="space-y-5" onSubmit={submitHandler}>
           <div className="space-y-2">
             <label className="text-sm font-medium tracking-wide text-gray-700">
-              Email
+              邮箱
             </label>
             <input
               className=" w-full rounded-lg border border-gray-300 px-4  py-2 text-base focus:border-green-400 focus:outline-none"
@@ -75,7 +75,7 @@ const LoginForm: React.FC = () => {
           </div>
           <div className="space-y-2">
             <label className="mb-5 text-sm font-medium tracking-wide text-gray-700">
-              Password
+              密码
             </label>
             <input
               className="w-full content-center rounded-lg border border-gray-300 px-4  py-2 text-base focus:border-green-400 focus:outline-none"
@@ -89,7 +89,7 @@ const LoginForm: React.FC = () => {
           <div className="flex items-center justify-between">
             <div className="text-sm">
               <a href="#" className="text-gray-400 hover:text-green-500">
-                Forgot your password?
+                忘记密码?
               </a>
             </div>
           </div>
@@ -101,21 +101,21 @@ const LoginForm: React.FC = () => {
                 { loading: loading }
               )}
             >
-              Sign in
+              登陆
             </button>
           </div>
         </form>
         <div className="pt-5 text-center text-xs text-gray-400">
           <span>
-            Copyright © 2021-2022
+            Copyright © 2021-2023
             <a
-              href="https://uavmatrix.com"
+              href="http://www.ipv6next.cn"
               rel=""
               target="_blank"
               title="Bernt Christian Egeland"
               className="text-green hover:text-slate-900 "
             >
-              @UAVMATRIX
+              @ipv6next
             </a>
           </span>
         </div>
